c124caea8a1c82094906d87e96f0ece2922b7f7c
NjugunaBrian <bryunnjuguna300@gmail.com>
2024-03-18 16:43:27 +0300

    custom morgan token

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const app = express()
 const morgan = require("morgan")
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+//custom function format for morgan
+morgan.token('body', (request, response) => {
+    if (request.body){
+        return JSON.stringify(request.body);
+    }
+    return '-';
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     {
